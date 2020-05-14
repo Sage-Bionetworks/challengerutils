0febb9be61ca51cbbf9894a4373b51c5848a79ad
Thomas Yu <thomasyu888@gmail.com>
2020-05-13 22:35:38 -0700

    Add tests

---
+++ tests/testthat/test-evaluation.R
@@ -0,0 +1,11 @@
+test_that("multiplication works", {
+  challengeutils <- import('challengeutils')
+  mock = mock(100)
+  stub(evaluation_queue_query, '.evaluation_queue_query', mock)
+  stub(evaluation_queue_query, "reticulate::iterate", list(list("objectId" = "333")))
+  results = evaluation_queue_query("syn", "select * from foo")
+  expect_called(mock, 1)
+  expect_args(mock, 1, "syn", "select * from foo")
+  ex
+})
+
