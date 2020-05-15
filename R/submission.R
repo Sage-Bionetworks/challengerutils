fda1f6603909f4bbbd331f136ffed44747b9310f
Thomas Yu <thomasyu888@gmail.com>
2020-05-14 22:26:16 -0700

    Test change submission

---
+++ R/submission.R
@@ -0,0 +1,25 @@
+#' Wraps challengeutils.utils.change_submission_status. Written for tests
+#'
+#' @param submissionid Id of a submission
+#' @param status Change a submision to this status. Default to RECEIVED.
+.change_submission_status <- function(submissionid, status = "RECEIVED") {
+  change_status <- challengeutils$utils$change_submission_status
+  change_status(syn = syn, submissionid = submissionid, status = status)
+}
+
+#' Changes a status of a submission
+#'
+#' @param submissionid Id of a submission
+#' @param status Change a submision to this status. Default to RECEIVED.
+#'
+#' @return Updated submisison status
+#' @examples
+#' library(challengerutils)
+#' use_condaenv('challenge')
+#' syn_login()
+#' new_status <- change_submission_status('9698553')
+#' @import reticulate
+#' @export
+change_submission_status <- function(submissionid, status = "RECEIVED") {
+  .change_submission_status(submissionid, status = status)
+}
