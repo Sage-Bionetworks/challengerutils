1f07b99bb73558b1d9b36548362bfdc37073c462
Robert Allaway <allaway@users.noreply.github.com>
2020-05-20 11:31:33 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,5 @@ syn <- NULL
 
   #if synapseclient has not already been imported, do it
   synapseclient <<- reticulate::import('synapseclient', delay_load = T)
-  syn <<- synapseclient$Synapse()
-
+  
 }
